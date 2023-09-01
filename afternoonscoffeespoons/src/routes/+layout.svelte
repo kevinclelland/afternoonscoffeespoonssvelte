593331703e9395cd5a84aed09ca83db1740bcb6f
kevinclelland <kevinclelland@gmail.com>
2023-09-01 20:39:33 +0200

    Initial commit

---
+++ afternoonscoffeespoons/src/routes/+layout.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+	import '../app.postcss';
+
+	
+	import { AppShell, AppBar, LightSwitch } from '@skeletonlabs/skeleton';
+</script>
+
+
+<AppShell regionPage="relative" slotPageHeader="sticky top-0 z-10">
+	<svelte:fragment slot="header">
+		<AppBar gridColumns="grid-cols-3" slotDefault="place-self-center" slotTrail="place-content-end">
+			<svelte:fragment slot="lead">(icon)</svelte:fragment>
+			<h1 class="text-4xl">Afternoons Coffeespoons</h1>
+			<svelte:fragment slot="trail"><LightSwitch /></svelte:fragment>
+		</AppBar>
+	</svelte:fragment>
+	<!-- (sidebarLeft) -->
+	<!-- (sidebarRight) -->
+	
+	<!-- Router Slot -->
+	<slot />
+	<!-- ---- / ---- -->
+	<svelte:fragment slot="pageFooter">Page Footer</svelte:fragment>
+	<!-- (footer) -->
+</AppShell>
