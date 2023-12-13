5df7fdca94e358f8f79f36e69f41a585f4e0586a
kevinclelland <kevinclelland@gmail.com>
2023-12-13 08:02:37 +0200

    Press Play

--- afternoonscoffeespoons/src/routes/+page.svelte
+++ afternoonscoffeespoons/src/routes/+page.svelte
@@ -56,7 +56,7 @@ if (vibeElement) {
 				</div>
 				<a href="/curators" title="Go to Curators"
 					class="relative inline-flex items-center justify-center px-8 py-4 text-lg font-bold text-white transition-all duration-200 bg-gray-900 font-pj rounded-xl focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-900"
-					role="button">Play
+					role="button">Press Play
 				</a>
 			</div>
 			
